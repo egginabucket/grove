8ca854125df020845cf94a3394f02e9d6dd44bef
egginabucket <hannah.a.einstein@gmail.com>
2022-12-24 18:54:58 -0800

    only show downloaded models

--- translator/meta.py
+++ translator/meta.py
@@ -8,7 +8,8 @@ from translator.models import SpacyLanguage, GoogleLanguage
 
 def get_supported_languages() -> Generator[LanguageTag, None, None]:
     wn_langs = wordnet.langs()
-    for spacy_lang in SpacyLanguage.objects.distinct("iso_lang"):
+    downloaded_langs = SpacyLanguage.objects.filter(downloaded=True)
+    for spacy_lang in downloaded_langs.distinct("iso_lang"):
         iso_lang = spacy_lang.iso_lang
         if iso_lang.part_3 in wn_langs:
             yield LanguageTag.objects.get_from_str(iso_lang.ietf).pref_tag
