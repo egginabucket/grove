a4ef78b2ecfa7fc07ffa927743df131cfe41af22
egginabucket <hannah.a.einstein@gmail.com>
2022-12-24 18:48:55 -0800

    remove redundant comments

--- grove/wsgi.py
+++ grove/wsgi.py
@@ -1,7 +1,5 @@
 """
-WSGI config for grove project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
+Exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
