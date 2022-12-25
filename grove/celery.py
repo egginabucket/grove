7c1de4417624bfaf809b706549df9daac7bfcaab
egginabucket <hannah.a.einstein@gmail.com>
2022-12-24 18:54:31 -0800

    formatting

--- grove/celery.py
+++ grove/celery.py
@@ -3,12 +3,11 @@
 # https://realpython.com/asynchronous-tasks-with-django-and-celery/
 
 
-
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_task.settings')
-app = Celery('celery_task')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "celery_task.settings")
+app = Celery("celery_task")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
